19e55e66ccf32a46235caa8bdd45b84540153534
TaniaLlujtaparo <155449861+TaniaLlujtaparo@users.noreply.github.com>
2024-01-26 15:49:55 +0000

    examen final

---
+++ src/app/components/pokemon.js
@@ -0,0 +1,29 @@
+"use client"
+import Image from "next/image";
+import style from "./pokemon.module.css";
+import { useEffect,useState } from "react";
+
+function Foto (){
+    const [pokemon,setPokemon]=useState("/vercel.svg");
+    const [estado, setEstado]=useState("esperando");
+    const [habilidad, setHabilidad] = useState("habilidad");
+    const url="https://pokeapi.co/api/v2/pokemon/110";
+    useEffect(()=>{
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>{
+            setPokemon(data.sprites.front_default),
+            setEstado(data.species.name),
+            setHabilidad(data.moves[0].move.name)
+            });
+    },[]);
+    return(
+        <div className={style.contenedor}>
+            <h1>{estado}</h1>
+            <Image src={pokemon} height={250} width={250}/>
+            <h1>{habilidad}</h1>
+        </div>
+    )
+}
+
+export default pokemon;
--- src/app/page.js
+++ src/app/page.js
@@ -5,36 +5,30 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
+
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
+          My Pokemon
+        Ivysaur&nbsp;
+          <code className={styles.code}></code>
         </p>
         <div>
-          <a
+          
+        <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+            
           </a>
         </div>
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+      <Image className={styles.logo}
+          src="/image 6.svg"
+          alt="imagen"
+          width={400}
+          height={400}
           priority
         />
       </div>
@@ -47,9 +41,20 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            Docs <span>-&gt;</span>
+            About
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <p>
+            type 
+          </p>
+          <p>
+            height
+          </p>
+          <p>
+            weight
+          </p>
+          <p>
+            abilities
+          </p>
         </a>
 
         <a
@@ -59,9 +64,9 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            Learn <span>-&gt;</span>
+            Stats
           </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+          <p>HP&nbsp;</p>
         </a>
 
         <a
@@ -70,23 +75,14 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Attack
+          </p>
+          <p>
+            Defense
+          </p>
+          <p>
+            Speed
           </p>
         </a>
       </div>
